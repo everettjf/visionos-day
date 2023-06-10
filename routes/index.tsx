bf5786f3f2ea5d2dcad1c4c4d8e24d9645c3d061
everettjf <everettjf@live.com>
2023-06-10 10:25:01 +0800

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,19 +5,18 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>visionos.day</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
         <img
           src="/logo.svg"
           class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
+          alt="visionos.day"
         />
         <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
+          YDNB
         </p>
-        <Counter start={3} />
+<!--         <Counter start={3} /> -->
       </div>
     </>
   );
